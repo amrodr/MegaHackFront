9f20f141a50911c29ed71a5466987cf11328ecb4
unknown <anderson.nunes17@etec.sp.gov.br>
2020-07-04 17:51:29 -0300

    Tela de ranking

--- src/app/ranking/ranking.page.ts
+++ src/app/ranking/ranking.page.ts
@@ -7,6 +7,13 @@ import { Component } from '@angular/core';
 })
 export class RankingPage {
 
+  private users = [ 
+    {id: 1, name: 'João', points: 1000, rank: 'Leader'},
+    {id: 2, name: 'Maria', points: 432, rank: 'Reader'},
+    {id: 3, name: 'Pâmela', points: 332, rank: 'Apprendice'},
+    {id: 4, name: 'Josué', points: 223, rank: 'Apprendice'},
+  ]
+
   constructor() {}
 
 }
