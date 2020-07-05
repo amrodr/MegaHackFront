1716915f1b275431b04770247a5b5f0a75f6b37d
unknown <anderson.nunes17@etec.sp.gov.br>
2020-07-05 01:38:41 -0300

    Perfil do usuario

--- src/app/profile/profile.page.ts
+++ src/app/profile/profile.page.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BookService } from 'src/services/book.service';
 
 @Component({
   selector: 'app-profile',
   templateUrl: 'profile.page.html',
   styleUrls: ['profile.page.scss']
 })
-export class ProfilePage {
+export class ProfilePage implements OnInit{
 
-  constructor() {}
+  books: any = [];
+  sliderConfiguration = {
+    spaceBetween: 10,
+    slidesPerView: 2.6
+  }
+  
+  constructor(private bookService: BookService) {}
+
+  ngOnInit() {
+    this.getBooks();
+  }
+
+  getBooks() {
+    this.books = this.bookService.getMyBooks();
+  }
 
 }
